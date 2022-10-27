26ac39f7854ea6f016fe86d2907a8fba09756537
Sparky <87631423+Sparky983@users.noreply.github.com>
2022-10-27 19:04:00 +1100

    fix: parameters aren't members

--- src/main/java/net/jailgens/mirror/Parameter.java
+++ src/main/java/net/jailgens/mirror/Parameter.java
@@ -1,5 +1,10 @@
 package net.jailgens.mirror;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.dataflow.qual.Pure;
+
+import java.util.Set;
+
 /**
  * A parameter.
  *
@@ -7,4 +12,23 @@ package net.jailgens.mirror;
  * @param <T> the type of the parameter.
  * @since 0.0.0
  */
-public interface Parameter<T> extends Annotated, Member, Typed<T> {}
+public interface Parameter<T> extends Annotated, Typed<T> {
+
+    /**
+     * Gets this member's name.
+     *
+     * @return this member's name.
+     * @since 0.0.0
+     */
+    @Pure
+    @NonNull String getName();
+
+    /**
+     * Gets all this member's modifiers.
+     *
+     * @return all this member's modifiers.
+     * @since 0.0.0
+     */
+    @Pure
+    @NonNull Set<@NonNull Modifier> getModifiers();
+}
