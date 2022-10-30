293a37d9a243bfe484f4b77f26d8ef3a6a38b3e5
Sparky <87631423+Sparky983@users.noreply.github.com>
2022-10-30 17:14:01 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
-    java
+    `java-library`
+    `maven-publish`
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
 }
 
 repositories {
@@ -19,8 +22,28 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.getByName<JavaCompile>("compileJava") {
-    options.compilerArgs.add("-parameters")
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "mirror"
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "sparky"
+            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://repo.sparky983.me/snapshots")
+            } else {
+                uri("https://repo.sparky983.me/releases")
+            }
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
 }
 
 tasks.getByName<JavaCompile>("compileTestJava") {
