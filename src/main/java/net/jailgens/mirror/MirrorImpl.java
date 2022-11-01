b1bb09db6fd83a1a40f595351586e6752185b06e
Sparky <87631423+Sparky983@users.noreply.github.com>
2022-11-01 11:20:30 +1100

    perf: add type definition cache (#7)
    Closes #3

--- src/main/java/net/jailgens/mirror/MirrorImpl.java
+++ src/main/java/net/jailgens/mirror/MirrorImpl.java
@@ -2,13 +2,27 @@ package net.jailgens.mirror;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 final class MirrorImpl implements Mirror {
 
+    private final Map<Class<?>, TypeDefinition<?>> typeDefinitionCache = new WeakHashMap<>();
+
+    @SuppressWarnings("unchecked")
     @Override
     public <T extends @NonNull Object> @NonNull TypeDefinition<T> reflect(
             final @NonNull Class<T> cls) {
 
-        return new TypeDefinitionImpl<>(cls);
+        final TypeDefinition<?> cachedTypeDefinition = typeDefinitionCache.get(cls);
+
+        if (cachedTypeDefinition != null) {
+            return (TypeDefinition<T>) cachedTypeDefinition;
+        }
+
+        final TypeDefinition<T> typeDefinition = new TypeDefinitionImpl<>(cls);
+        typeDefinitionCache.put(cls, typeDefinition);
+        return typeDefinition;
     }
 
     static final class BuilderImpl implements Builder {
