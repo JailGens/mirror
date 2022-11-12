08ea5ac2ca6358817eb44ce9868f2667c5cb6a95
Sparky <87631423+Sparky983@users.noreply.github.com>
2022-11-12 17:15:02 +1100

    feat: add proxies (#32)

--- src/main/java/net/jailgens/mirror/Mirror.java
+++ src/main/java/net/jailgens/mirror/Mirror.java
@@ -2,7 +2,11 @@ package net.jailgens.mirror;
 
 import org.checkerframework.checker.lock.qual.NewObject;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.common.returnsreceiver.qual.This;
 import org.checkerframework.dataflow.qual.SideEffectFree;
+
+import java.lang.reflect.Proxy;
 
 /**
  * Reflects types.
@@ -37,11 +41,38 @@ public interface Mirror {
             @NonNull Class<@NonNull T> cls);
 
     /**
+     * Creates a proxy for the specified type.
+     *
+     * @param type the type.
+     * @param handler the handler.
+     * @return the proxy.
+     * @param <T> the type of the type.
+     * @throws IllegalArgumentException if any restrictions defined in {@link Proxy} are
+     * violated.
+     * @throws NullPointerException if {@code type} or {@code handler} is {@code null}.
+     * @since 0.2.0
+     */
+    <T extends @NonNull Object> @NonNull T createProxy(@NonNull Class<T> type,
+                                                       @NonNull InvocationHandler<T> handler);
+
+    /**
      * A {@link Mirror} builder.
      *
      * @since 0.0.0
      */
     interface Builder {
+
+        /**
+         * Sets the class loader to use.
+         * <p>
+         * If the specified class loader is {@code null}, or none is specified, the system class
+         * loader will be used.
+         *
+         * @param classLoader the class loader to use.
+         * @return this builder (for chaining).
+         * @since 0.2.0
+         */
+        @NonNull @This Builder classLoader(@Nullable ClassLoader classLoader);
 
         /**
          * Builds the mirror.
--- src/main/java/net/jailgens/mirror/MirrorImpl.java
+++ src/main/java/net/jailgens/mirror/MirrorImpl.java
@@ -1,8 +1,12 @@
 package net.jailgens.mirror;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.common.returnsreceiver.qual.This;
 
+import java.lang.reflect.Proxy;
 import java.util.Map;
+import java.util.Objects;
 import java.util.WeakHashMap;
 
 /**
@@ -17,10 +21,22 @@ final class MirrorImpl implements Mirror {
      */
     private static class Instance {
 
-        private static final Mirror INSTANCE = new MirrorImpl();
+        private static final @NonNull Mirror INSTANCE = new MirrorImpl(DEFAULT_CLASS_LOADER);
     }
 
-    private final Map<Class<?>, TypeDefinition<?>> typeDefinitionCache = new WeakHashMap<>();
+    private static final @NonNull ClassLoader DEFAULT_CLASS_LOADER = ClassLoader.getSystemClassLoader();
+    private static final @NonNull Object @NonNull [] EMPTY_OBJECT_ARRAY = new Object[0];
+
+    private final @NonNull Map<
+            @NonNull Class<? extends @NonNull Object>,
+            @NonNull TypeDefinition<? extends @NonNull Object>
+            > typeDefinitionCache = new WeakHashMap<>();
+    private final @NonNull ClassLoader classLoader;
+
+    private MirrorImpl(final @NonNull ClassLoader classLoader) {
+
+        this.classLoader = classLoader;
+    }
 
     @SuppressWarnings("unchecked")
     @Override
@@ -38,12 +54,45 @@ final class MirrorImpl implements Mirror {
         return typeDefinition;
     }
 
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T extends @NonNull Object> @NonNull T createProxy(
+            final @NonNull Class<@NonNull T> type,
+            final @NonNull InvocationHandler<T> handler) {
+
+        Objects.requireNonNull(type, "type cannot be null");
+        Objects.requireNonNull(handler, "handler cannot be null");
+
+        final TypeDefinition<T> typeDefinition = reflect(type);
+
+        return (T) Proxy.newProxyInstance(classLoader,
+                new Class<?>[] {type},
+                (proxy, method, args) -> handler.invoke((T) proxy,
+                        new MethodImpl<>(typeDefinition, method),
+                        args == null ?
+                                EMPTY_OBJECT_ARRAY :
+                                args));
+    }
+
     static final class BuilderImpl implements Builder {
+
+        private ClassLoader classLoader;
+
+        @Override
+        public @NonNull @This Builder classLoader(@Nullable ClassLoader classLoader) {
+
+            this.classLoader = classLoader;
+            return this;
+        }
 
         @Override
         public @NonNull Mirror build() {
 
-            return Instance.INSTANCE;
+            if (classLoader == null) {
+                return Instance.INSTANCE;
+            }
+
+            return new MirrorImpl(classLoader);
         }
     }
 }
